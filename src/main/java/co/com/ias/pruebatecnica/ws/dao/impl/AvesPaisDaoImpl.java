f1eb158b15a02c48c91ad225e46355841a2837c1
Gerlin Orlando Torres Saavedra <gtorress@everis.com>
2017-06-11 18:08:37 -0500

    fix: sobreescribio el metodo findById AvesPais

--- src/main/java/co/com/ias/pruebatecnica/ws/dao/impl/AvesPaisDaoImpl.java
+++ src/main/java/co/com/ias/pruebatecnica/ws/dao/impl/AvesPaisDaoImpl.java
@@ -19,13 +19,16 @@ import co.com.ias.pruebatecnica.ws.domain.AvesPais;
  */
 @Repository("AvesPaisDaoImpl")
 public class AvesPaisDaoImpl extends ADaoGenericImpl<AvesPais> implements IAvesPaisDao {
-	
-	/* (non-Javadoc)
-	 * @see co.com.ias.pruebatecnica.ws.dao.IAvesPaisDao#findByAve(co.com.ias.pruebatecnica.ws.domain.Ave)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see co.com.ias.pruebatecnica.ws.dao.IAvesPaisDao#findByAve(co.com.ias.
+	 * pruebatecnica.ws.domain.Ave)
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
-	public List<AvesPais> findByAve(Ave ave){
+	public List<AvesPais> findByAve(Ave ave) {
 		List<AvesPais> avesPais;
 		StringBuilder hql = new StringBuilder("FROM AvesPais ap WHERE 1=1");
 		if (null != ave)
@@ -34,6 +37,24 @@ public class AvesPaisDaoImpl extends ADaoGenericImpl<AvesPais> implements IAvesP
 		if (null != ave)
 			query.setParameter("idAve", ave.getCodigo());
 		avesPais = query.list().isEmpty() ? null : query.list();
+		return avesPais;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * co.com.ias.pruebatecnica.ws.dao.impl.ADaoGenericImpl#findById(java.lang.
+	 * Object)
+	 */
+	@Override
+	public AvesPais findById(AvesPais avesPaises) {
+		AvesPais avesPais;
+		StringBuilder hql = new StringBuilder("FROM AvesPais ap WHERE ap.idPais = :idPais AND ap.idAve = :idAve");
+		Query query = super.getSessionFactory().getCurrentSession().createQuery(hql.toString());
+		query.setParameter("idAve", avesPaises.getIdAve());
+		query.setParameter("idPais", avesPaises.getIdPais());
+		avesPais = query.list().isEmpty() ? null : (AvesPais) query.list().get(0);
 		return avesPais;
 	}
 }
