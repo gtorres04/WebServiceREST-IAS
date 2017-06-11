b8426924d5b7a7563e7d877d68abaaf3e0982561
Gerlin Orlando Torres Saavedra <gtorress@everis.com>
2017-06-11 01:28:15 -0500

    feat: se agrego el casteo de la Zona

--- src/main/java/co/com/ias/pruebatecnica/ws/dto/ZonaDto.java
+++ src/main/java/co/com/ias/pruebatecnica/ws/dto/ZonaDto.java
@@ -5,6 +5,10 @@ package co.com.ias.pruebatecnica.ws.dto;
 
 import java.io.Serializable;
 
+import org.springframework.beans.BeanUtils;
+
+import co.com.ias.pruebatecnica.ws.domain.Zona;
+
 /**
  * @author gtorress
  *
@@ -46,5 +50,19 @@ public class ZonaDto implements Serializable{
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	/**
+	 * Retorna un Dto a partir de un dominio.
+	 * @param zona
+	 * @return
+	 */
+	public static ZonaDto getDto(Zona zona) {
+		ZonaDto zonaDto = null;
+		if(null != zona){
+			zonaDto = new ZonaDto();
+			BeanUtils.copyProperties(zona, zonaDto);
+		}
+		return zonaDto;
 	}	
 }
